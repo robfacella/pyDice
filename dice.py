2681aa50ad081b2858bea8df1004e07fdfa29000
Rob Facella <rob.facella@gmail.com>
2020-05-26 19:18:36 -0400

    integer args as n sided die

--- dice.py
+++ dice.py
@@ -7,7 +7,14 @@ if (len(sys.argv) < 2):
   print ("Rolling die: ")
   print (random.randint(1,6))
 #else for rach arg roll a die of arg[i] sides if the arg is a number else skip it.
-#else:
-#  for
-#   print ("Rolling d%s: ")
+else:
+  for sidedDie in sys.argv:
+    if (sidedDie == "dice.py"):
+      print (" ")
+      #Don't want to try to turn "dice.py" into an integer.
+    else:
+      print ("Rolling %s sided die: " % sidedDie)
+      sides = int(sidedDie)
+      print (random.randint(1,sides))
+      print " "
 
